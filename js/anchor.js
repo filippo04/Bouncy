dced936ba236d53e84398dcd83cbc163e1d1cde1
filippo04 <filippo0491@gmail.com>
2017-04-27 19:00:12 +0300

    Bouncy

---
+++ js/anchor.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $(".item, .hd-menu, .tc").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1000);
+    });
+});
